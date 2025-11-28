89847e59e3b397be0e541e0b18deb68c3c05a068
KilluaZ01 <raiarik97@gmail.com>
2025-11-28 15:40:12 +0545

    Battle Logic Added

--- bot/macros/fight_actions.py
+++ bot/macros/fight_actions.py
@@ -22,12 +22,14 @@ def basic_attack_combo_dodge(adb_address, *args):
         adb_address: ADB address of the device
         args: Additional arguments (not used)
     """
-    for i in range(5):
+    tap_macro(adb_address, 951, 642)  # Dodge action
+    time.sleep(0.5)
+    for i in range(10):
         tap_macro(adb_address, 1068, 538)
         time.sleep(0.5)
     tap_macro(adb_address, 951, 642)  # Dodge action
-    time.sleep(1)
-    for i in range(5):
+    time.sleep(0.5)
+    for i in range(10):
         tap_macro(adb_address, 1068, 538)
         time.sleep(0.5)
     swipe_macro(adb_address, 1068, 533, 1068, 533, 2500)
