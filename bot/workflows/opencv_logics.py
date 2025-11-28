02f3d45a77c90321c2d7864120036f758c3569e6
KilluaZ01 <raiarik97@gmail.com>
2025-11-28 15:40:42 +0545

    Macro Added for Gatcha Summon

--- bot/workflows/opencv_logics.py
+++ bot/workflows/opencv_logics.py
@@ -32,18 +32,18 @@ def check_fight_skip(adb_address, *args):
             return True
         basic_attack_combo_dodge(adb_address)
         basic_attack_combo_dodge(adb_address)
-        tap_macro(944, 493)  # Special Attack
+        tap_macro(adb_address, 944, 493)  # Special Attack
         time.sleep(1)
 
 def check_counter(adb_address, *args):
     template_path = f"{TEMPLATE_DIR}/counter.png"
 
     for i in range(5):
-        if check_template(adb_address, template_path, threshold=0.8):
+        if not check_template(adb_address, template_path, threshold=0.8):
             return True
         basic_attack_combo_dodge(adb_address)
         basic_attack_combo_dodge(adb_address)
-        tap_macro(944, 493)  # Special Attack
+        tap_macro(adb_address, 944, 493)  # Special Attack
         time.sleep(1)
 
 def draw_10x_till_1x(adb_address, *args):
@@ -51,30 +51,50 @@ def draw_10x_till_1x(adb_address, *args):
     template_path_require = f"{TEMPLATE_DIR}/token_require.png"
     template_path_close = f"{TEMPLATE_DIR}/close_button.png"
 
-
-    tap_macro(1146, 658) # Gacha 10x pull
+    tap_macro(adb_address, 1146, 658) # Gacha 10x pull
     time.sleep(3)
 
     for i in range(15):
         if check_template(adb_address, template_path_skip, threshold=0.8):
-            tap_macro(1180, 52)  # Skip
+            tap_macro(adb_address, 1180, 52)  # Skip
             time.sleep(2)
-            tap_macro(1180, 52)  # Skip
+            tap_macro(adb_address, 1180, 52)  # Skip
             time.sleep(2)
         else:
-            tap_macro(640, 664)  # Tap to close
+            tap_macro(adb_address, 640, 664)  # Tap to close
             time.sleep(3)
-            tap_macro(1146, 658)  # Gacha 10x pull
+            tap_macro(adb_address,  1146, 658)  # Gacha 10x pull
             time.sleep(3)
 
             if check_template(adb_address, template_path_close, threshold=0.8):
                 if check_template(adb_address, template_path_require, threshold=0.8):
-                    tap_macro(978, 203)  # Close token require
+                    tap_macro(adb_address, 978, 203)  # Close token require
                     time.sleep(3)
                     return True
                 
-                tap_macro(868, 512)  # Confirm
+                tap_macro(adb_address, 868, 512)  # Confirm
                 time.sleep(3)
+
+def draw_till_next(adb_address, *args):
+    template_path_skip = f"{TEMPLATE_DIR}/skip_button.png"
+    template_path_close = f"{TEMPLATE_DIR}/close_button.png"
+
+    for i in range(15):
+        if check_template(adb_address, template_path_skip, threshold=0.8):
+            tap_macro(adb_address, 1180, 52)  # Skip
+            time.sleep(2)
+            tap_macro(adb_address, 1180, 52)  # Skip
+            time.sleep(2)
+        else:
+            tap_macro(adb_address, 640, 664)  # Tap to close
+            time.sleep(3)
+            tap_macro(adb_address,  1146, 658)  # Gacha 10x pull
+            time.sleep(3)
+
+            if check_template(adb_address, template_path_close, threshold=0.8):
+                tap_macro(adb_address, 978, 203)  # Close token require
+                time.sleep(3)
+                return True
 
 def draw_1x_till_0(adb_address, *args):
     template_path_skip = f"{TEMPLATE_DIR}/skip_button.png"
@@ -86,23 +106,23 @@ def draw_1x_till_0(adb_address, *args):
 
     for i in range(15):
         if check_template(adb_address, template_path_skip, threshold=0.8):
-            tap_macro(1180, 52)  # Skip
+            tap_macro(adb_address, 1180, 52)  # Skip
             time.sleep(2)
-            tap_macro(1180, 52)  # Skip
+            tap_macro(adb_address, 1180, 52)  # Skip
             time.sleep(2)
         else:
-            tap_macro(640, 664)  # Tap to close
+            tap_macro(adb_address, 640, 664)  # Tap to close
             time.sleep(3)
-            tap_macro(845, 657)  # Gacha 1x pull
+            tap_macro(adb_address, 845, 657)  # Gacha 1x pull
             time.sleep(3)
 
             if check_template(adb_address, template_path_close, threshold=0.8):
                 if check_template(adb_address, template_path_require, threshold=0.8):
-                    tap_macro(978, 203)  # Close token require
+                    tap_macro(adb_address, 978, 203)  # Close token require
                     time.sleep(3)
                     return True
                 
-                tap_macro(868, 512)  # Confirm
+                tap_macro(adb_address, 868, 512)  # Confirm
                 time.sleep(3)
 
 def find_more(adb_address, *args):
@@ -111,7 +131,7 @@ def find_more(adb_address, *args):
     coords = find_coordinates(adb_address, template_path, threshold=0.8)
     
     if coords:
-        tap_macro(coords[0], coords[1])
+        tap_macro(adb_address, coords[0], coords[1])
         time.sleep(1)
         return True
 
@@ -120,3 +140,11 @@ def final_screenshot(instance_name, *args):
     ts = time.strftime("%H%M%S")
     FILENAME = f"Day1_{instance_name}_{ts}.png"
     take_screenshot(instance_name, DIR, FILENAME)
+
+def check_counter_text(adb_address, *args):
+    template_path = f"{TEMPLATE_DIR}/counter_text.png"
+
+    for i in range(3):
+        if check_template(adb_address, template_path, threshold=0.8):
+            return True
+        time.sleep(5)
